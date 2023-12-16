d57f034943fba5a03ef1b7636d6718c7eaf84bc7
anirudh716 <anirudhpandey244@gmail.com>
2023-12-16 14:22:57 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,8 +1,34 @@
 import cv2 as cv
 img = cv.imread('Photos/photo3.jpg')
-cv.imshow('image', img)
+# cv.imshow('image', img)
 
-
+#converting to grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('gray', gray)
+# cv.imshow('gray', gray)
+
+#blur
+blurred = cv.GaussianBlur(img,(13,13),cv.BORDER_DEFAULT)
+# cv.imshow('blur', blurred)
+
+#edge cascade
+canny = cv.Canny(img, 125,175)
+# cv.imshow('cnny', canny)
+
+#dilating the images
+dilated = cv.dilate(canny, (3,3), iterations=3)
+# cv.imshow('dilated', dilated)
+
+eroded = cv.erode(dilated, (3,3), iterations=3)
+# cv.imshow('erode', eroded)
+
+imag = cv.imread('Photos/photo1.jpg')
+# cv.imshow('image', imag)
+
+#resize
+resized = cv.resize(imag, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('resized', resized)
+
+#cropping
+croped = imag[50:200, 50:400]
+cv.imshow("crop", croped)
 cv.waitKey()
